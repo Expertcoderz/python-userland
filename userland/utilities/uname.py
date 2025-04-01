1018d2f33904dfe10edcc8850ea3450d37d41675
Expertcoderz <expertcoderzx@gmail.com>
2025-04-01 15:30:48 +0000

    uname: Support long options

--- userland/utilities/uname.py
+++ userland/utilities/uname.py
@@ -12,15 +12,60 @@ parser = lib.create_parser(
     description="Print system information.",
 )
 
-parser.add_option("-a", action="store_true", help="print all")
-parser.add_option("-m", action="store_true", help="machine hardware type")
-parser.add_option("-n", action="store_true", help="hostname")
-parser.add_option("-r", action="store_true", help="kernel release")
-parser.add_option("-s", action="store_true", help="kernel name (default)")
-parser.add_option("-v", action="store_true", help="kernel version")
-parser.add_option("-p", action="store_true", help="processor type")
-parser.add_option("-i", action="store_true", help="hardware platform (unimplemented)")
-parser.add_option("-o", action="store_true", help="operating system (unimplemented)")
+parser.add_option(
+    "-a",
+    "--all",
+    action="store_true",
+    help="print all",
+)
+parser.add_option(
+    "-s",
+    "--kernel-name",
+    action="store_true",
+    help="print kernel name (default)",
+)
+parser.add_option(
+    "-n",
+    "--nodename",
+    action="store_true",
+    help="print hostname",
+)
+parser.add_option(
+    "-r",
+    "--kernel-release",
+    action="store_true",
+    help="print kernel release",
+)
+parser.add_option(
+    "-v",
+    "--kernel-version",
+    action="store_true",
+    help="print kernel version",
+)
+parser.add_option(
+    "-m",
+    "--machine",
+    action="store_true",
+    help="print machine hardware type",
+)
+parser.add_option(
+    "-p",
+    "--processor",
+    action="store_true",
+    help="print processor type (unimplemented)",
+)
+parser.add_option(
+    "-i",
+    "--hardware-platform",
+    action="store_true",
+    help="print hardware platform (unimplemented)",
+)
+parser.add_option(
+    "-o",
+    "--operating-system",
+    action="store_true",
+    help="print operating system (unimplemented)",
+)
 
 
 @lib.command(parser)
